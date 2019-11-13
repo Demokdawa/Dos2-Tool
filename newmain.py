653c9fcce2211e744f66709a38c01ce75ec92755
Demokdawa <Demokdawa@yahoo.fr>
2019-11-13 14:25:01 +0100

    test

--- newmain.py
+++ newmain.py
@@ -5,16 +5,41 @@ txt2 = open("english.xml", "r", encoding="utf8")
 txt3 = open("french.xml", "r", encoding="utf8")
 file1 = txt1.read().split("\n")
 file2 = txt2.read()
-ctn = 1
+file3 = txt3.read()
 
-for line in file1:
-    # print(ctn)
-    val = re.search('">(.*)</content>', line)
-    ctn = ctn + 1
-    if val is not None:
-        val_grp = val.group(1)
-        match = re.search(str(val_grp), file2)
-        if match is not None:
-            print(val_grp + ' || ' + str(match))
-        else:
-            print(match)
+
+def retrieve_trad(uid):
+    match = re.search(uid, file3)
+    if match is not None:
+        pass
+        # print('Searching for ' + uid + ' SUCESS')
+    else:
+        pass
+        # print('Searching for ' + uid + ' FAILED')
+
+
+# Check presence of string in file2, an pass uid if match
+def check_for_existing(str_base):
+    pat = re.escape('">' + str_base + '</content>')
+    match = re.search(pat, file2)
+    if match is not None:
+        match3 = re.search(f'<content contentuid="(.*){pat}', file2, re.MULTILINE)
+        if match3 is not None:
+            uid = match3.group(1)
+            retrieve_trad(uid)
+
+
+# Parse line by line in file 1
+def check_lines():
+    for line in file1:
+        raw_val = re.search('">(.*)</content>', line)
+        if raw_val is not None:
+            val = raw_val.group(1)
+            if val == '':
+                pass
+            if val is not None:
+                check_for_existing(val)
+
+
+if __name__ == '__main__':
+    check_lines()
